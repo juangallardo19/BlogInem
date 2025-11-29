5698f10d08aca56ed694b8766fbef0ce2d51594c
juangallardo19 <161619122+juangallardo19@users.noreply.github.com>
2025-11-29 15:02:32 +0000

    cambio dis3no

--- Script.js
+++ Script.js
@@ -419,11 +419,13 @@ function generatePublicationHTML(pub) {
                 </div>
                 <div class="publication-actions">
                     ${deleteButton}
+                    ${isAdminMode ? `
                     <button class="folder-btn" onclick="openFolder('${pub.folderUrl}')" title="Ver archivos">
                         <svg class="icon-btn-small" viewBox="0 0 24 24" fill="none" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"/>
                         </svg>
                     </button>
+                    ` : ''}
                 </div>
             </div>
             
