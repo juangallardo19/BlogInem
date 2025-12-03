58bd3447781616febc4f7c845e2cd07191f6609f
juangallardo19 <juangallardocsfn@gmail.com>
2025-12-02 23:24:14 -0500

    permitir eliminar

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@
 // FRONTEND ARREGLADO PARA MEDIOS
 // ========================================
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbwJ6brd2mdheu5mcue5XRSDRg9VpNQ8_xAQrbgPkeFCPVGJUSm88hCKmfmvVp7ruomb/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwiVEWq6dybGTUZAVc1G_vCWd-1i3ySzWZjLWbDLC4l6lYj58vmlFSrFj8GMfRvlXMq/exec';
 const MAX_AUDIO_SIZE = 10 * 1024 * 1024; // 10MB
 const MAX_VIDEO_SIZE = 30 * 1024 * 1024; // 30MB
 
